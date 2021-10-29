0032325904dc8b761927e000ab45866dfa34b741
HEHABUSTb <msanvold@gmail.com>
2021-10-29 15:55:41 +0300

    minor typo change in function

--- test_items.py
+++ test_items.py
@@ -2,7 +2,7 @@ import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser,language):
+def test_guest_should_see_basket(browser,language):
     link = f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/"
     browser.get(link)
     basket = browser.find_element_by_css_selector('button.btn:nth-child(3)')
