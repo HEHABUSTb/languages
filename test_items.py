e785ade7abbe05a24b9f4d6bf04a458a85cf1c55
HEHABUSTb <msanvold@gmail.com>
2021-10-29 15:19:07 +0300

    add conftest.py and test_items.py add language parametr for tests

---
+++ test_items.py
@@ -0,0 +1,11 @@
+import time
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_guest_should_see_login_link(browser,language):
+    link = f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/"
+    browser.get(link)
+    basket = browser.find_element_by_css_selector('button.btn:nth-child(3)')
+    print(basket)
+    assert basket is not None, 'We cant find basket button'
+    time.sleep(10)
